f748da9304e599e943a11594d5b34c74824f9f69
NoMad42 <nomad42@nomad42.ru>
2023-08-12 21:22:52 +0300

    Container With Most Water

---
+++ php/TwoPointers/ContainerWithMostWater.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ContainerWithMostWater;
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function maxArea($height) {
+        $maxArea = 0;
+        $left = 0;
+        $right = count($height) - 1;
+
+        while ($left < $right) {
+            $area = ($right - $left) * min($height[$left], $height[$right]);
+            $maxArea = max($area, $maxArea);
+
+            if ($height[$left] < $height[$right]) {
+                $left++;
+            } else {
+                $right--;
+            }
+        }
+        return $maxArea;
+    }
+}
---
+++ php/TwoPointers/ContainerWithMostWaterTest.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once "ContainerWithMostWater.php";
+
+$sol = new ContainerWithMostWater\Solution();
+
+assert(49 === $sol->maxArea([1,8,6,2,5,4,8,3,7]));
+assert(1 === $sol->maxArea([1,1]));
